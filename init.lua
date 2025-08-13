88a6d7cab3be2a342a9786aa01b3739b89a4fc15
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2025-08-13 14:00:59 +0200

    Lexpole navigation

--- init.lua
+++ init.lua
@@ -100,19 +100,51 @@ vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without yankin
 vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
 
+function ListAndSelectBuffers()
+  -- Get a list of all listed buffers
+  local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+
+  -- Create a table to store buffer information for the selection UI
+  local buf_choices = {}
+  for _, buf in ipairs(bufs) do
+    table.insert(buf_choices, string.format("%d: %s", buf.bufnr, vim.fn.fnamemodify(buf.name, ":t")))
+  end
+
+  -- Use vim.ui.select to present the choices to the user
+  vim.ui.select(buf_choices, {
+    prompt = "Select a buffer:",
+  }, function(choice)
+    if not choice then return end
+    -- Extract the buffer number from the choice string
+    local bufnr = tonumber(string.match(choice, "^%d+"))
+    if bufnr then
+      vim.api.nvim_set_current_buf(bufnr)
+    end
+  end)
+end
+
+-- Create a keymap to call the function
+vim.keymap.set('n', '<leader>bl', ListAndSelectBuffers, { desc = 'List and select buffers' })
+
 -- Better window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
+-- Better window navigation using arraows
+vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Move to right window" })
+
 -- Splitting & Resizing
 vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+vim.keymap.set("n", "<S-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<S-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<S-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<S-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
 
 -- Move lines up/down
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
@@ -131,7 +163,27 @@ vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Indent left and reselect" })
 vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent right and reselect" })
 
 -- Quick file navigation
-vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open file explorer" })
+vim.g.netrw_banner = 0           -- Hide the distracting banner
+vim.g.netrw_liststyle = 3        -- Set tree-style view as the default
+vim.g.netrw_browse_split = 0     -- Open in the same window, we will override this
+vim.g.netrw_winsize = 15         -- Set window size to in %
+
+-- Function to toggle the file explorer on the left
+function ToggleNetrw()
+  -- Check if a netrw buffer is open in the current tab
+  local netrw_win_id = vim.fn.bufwinnr('^netrw')
+
+  if netrw_win_id ~= -1 then
+    -- If netrw is open, close its window
+    vim.cmd('Lexplore!')
+  else
+    -- If netrw is not open, open it on the left
+    vim.cmd('Lexplore')
+  end
+end
+
+vim.keymap.set("n", "<leader>e", ToggleNetrw, { desc = "Toggle file explorer" })
+
 vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find file" })
 
 -- Better J behavior
